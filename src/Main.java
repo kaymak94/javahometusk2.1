223dc5629568c93a2c9012eb3648c87bf1e1f22f
Каймакчи Александр <kaymakchiao@mail.ru>
2022-05-08 22:06:14 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int money = 275;
+        int miles = 20;
+        int result = money/miles;
+        System.out.println("Количество начисленных миль:" + result);
+    }
+
+}
